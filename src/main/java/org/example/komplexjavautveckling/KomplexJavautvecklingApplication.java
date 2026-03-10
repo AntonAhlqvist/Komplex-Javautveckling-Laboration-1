0a815f0c0ec5016e3830fa4c0d61b6f8f2a8f516
Anton Ahlqvist <anton.ahlqvist@iths.se>
2026-03-10 20:51:15 +0100

    Initial commit

---
+++ src/main/java/org/example/komplexjavautveckling/KomplexJavautvecklingApplication.java
@@ -0,0 +1,13 @@
+package org.example.komplexjavautveckling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomplexJavautvecklingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KomplexJavautvecklingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/komplexjavautveckling/KomplexJavautvecklingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.komplexjavautveckling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomplexJavautvecklingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
