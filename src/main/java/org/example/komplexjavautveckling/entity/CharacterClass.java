828479c8c790edc3101edbc154d3a5b9eda95e8f
Anton Ahlqvist <anton.ahlqvist@iths.se>
2026-03-12 13:16:12 +0100

    Add: Character enum

---
+++ src/main/java/org/example/komplexjavautveckling/entity/CharacterClass.java
@@ -0,0 +1,21 @@
+package org.example.komplexjavautveckling.entity;
+
+public enum CharacterClass {
+
+    MYSTIC("Mystiker", 140),
+    BLADEBEARER("Klingbärare", 100),
+    ARROWMASTER("Pilmästare", 160),
+    SHADOWDANCER("Skuggdansare", 200);
+
+    private final String displayName;
+    private final int startingPesetas;
+
+    CharacterClass(String displayName, int startingPesetas) {
+
+        this.displayName = displayName;
+        this.startingPesetas = startingPesetas;
+    }
+
+    public String getDisplayName() { return displayName; }
+    public int getStartingPesetas() { return startingPesetas; }
+}
