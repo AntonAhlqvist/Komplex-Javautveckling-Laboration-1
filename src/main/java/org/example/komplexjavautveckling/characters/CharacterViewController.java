8545ef8ab51ef3f8fc27f7d6a6c8f60e3d8a682a
Anton Ahlqvist <anton.ahlqvist@iths.se>
2026-03-13 06:10:42 +0100

    Add: CharacterViewController

---
+++ src/main/java/org/example/komplexjavautveckling/characters/CharacterViewController.java
@@ -0,0 +1,41 @@
+package org.example.komplexjavautveckling.characters;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CharacterViewController {
+
+    private final CharacterService characterService;
+
+    public CharacterViewController(CharacterService characterService) {
+        this.characterService = characterService;
+    }
+
+    @GetMapping("/characters/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("character", new CreateCharacterDTO());
+        model.addAttribute("classes", characterService.getClasses());
+        return "create-character";
+    }
+
+    @PostMapping("/characters/create")
+    public String createCharacter(@ModelAttribute CreateCharacterDTO characterDto) {
+        characterService.createCharacter(characterDto);
+        return "redirect:/characters/next-step";
+    }
+
+    @GetMapping("/characters/next-step")
+    public String nextStep() {
+        return "next-step";
+    }
+
+    @GetMapping("/characters/classes")
+    public String showClasses(Model model) {
+        model.addAttribute("classes", characterService.getClasses());
+        return "classes";
+    }
+}
