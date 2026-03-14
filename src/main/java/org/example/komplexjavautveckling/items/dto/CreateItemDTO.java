23b877b5454e29c72a4a5b8f99884e839f32a034
Anton Ahlqvist <anton.ahlqvist@iths.se>
2026-03-14 23:33:39 +0100

    Update: add validation constraints to the item DTO fields

--- src/main/java/org/example/komplexjavautveckling/items/dto/CreateItemDTO.java
+++ src/main/java/org/example/komplexjavautveckling/items/dto/CreateItemDTO.java
@@ -1,25 +1,57 @@
 package org.example.komplexjavautveckling.items.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.example.komplexjavautveckling.items.enums.ItemType;
 
 public class CreateItemDTO {
 
+    @NotBlank(message = "Namn krävs")
     private String name;
+
+    @NotNull(message = "Föremålstyp krävs")
     private ItemType type;
+
+    @PositiveOrZero(message = "Skada måste vara positiv eller noll")
     private int damage;
+
+    @Positive(message = "Vikt måste vara positiv")
     private double weight;
 
-    public CreateItemDTO() {}
+    public CreateItemDTO() {
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
 
-    public ItemType getType() { return type; }
-    public void setType(ItemType type) { this.type = type; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public int getDamage() { return damage; }
-    public void setDamage(int damage) { this.damage = damage; }
+    public ItemType getType() {
+        return type;
+    }
 
-    public double getWeight() { return weight; }
-    public void setWeight(double weight) { this.weight = weight; }
+    public void setType(ItemType type) {
+        this.type = type;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
