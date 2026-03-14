c4be1d6c8d923ded9aac7c69956ddd81016f89af
Anton Ahlqvist <anton.ahlqvist@iths.se>
2026-03-14 04:42:54 +0100

    Refactor: Update ItemMapper to support new Item fields and DTO changes

--- src/main/java/org/example/komplexjavautveckling/items/ItemMapper.java
+++ src/main/java/org/example/komplexjavautveckling/items/ItemMapper.java
@@ -1,31 +1,49 @@
 package org.example.komplexjavautveckling.items;
 
+import org.example.komplexjavautveckling.items.dto.CreateItemDTO;
+import org.example.komplexjavautveckling.items.dto.ItemDTO;
+import org.example.komplexjavautveckling.items.dto.UpdateItemDTO;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class ItemMapper {
 
     public Item toEntity(CreateItemDTO dto) {
+
         Item item = new Item();
+
         item.setName(dto.getName());
-        item.setPrice(dto.getPrice());
         item.setType(dto.getType());
+        item.setDamage(dto.getDamage());
+        item.setWeight(dto.getWeight());
+
+        item.setCreatedDate(LocalDate.now());
+
         return item;
     }
 
     public void updateEntity(Item item, UpdateItemDTO dto) {
+
         item.setName(dto.getName());
-        item.setPrice(dto.getPrice());
         item.setType(dto.getType());
+        item.setDamage(dto.getDamage());
+        item.setWeight(dto.getWeight());
     }
 
     public ItemDTO toDTO(Item item) {
+
         ItemDTO dto = new ItemDTO();
+
         dto.setId(item.getId());
         dto.setName(item.getName());
-        dto.setPrice(item.getPrice());
         dto.setType(item.getType());
+        dto.setDamage(item.getDamage());
+        dto.setWeight(item.getWeight());
+        dto.setPrice(item.getPrice());
         dto.setCreatedDate(item.getCreatedDate());
+
         return dto;
     }
 }
