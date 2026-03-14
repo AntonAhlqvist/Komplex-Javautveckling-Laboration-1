fcb885e1ce98b4337437fbd8348cfe2cb56ab1f7
Anton Ahlqvist <anton.ahlqvist@iths.se>
2026-03-14 12:29:45 +0100

    Refactor: replace String type with ItemType enum in ItemRepository for improved type safety

--- src/main/java/org/example/komplexjavautveckling/items/ItemRepository.java
+++ src/main/java/org/example/komplexjavautveckling/items/ItemRepository.java
@@ -1,12 +1,13 @@
 package org.example.komplexjavautveckling.items;
 
+import org.example.komplexjavautveckling.items.enums.ItemType;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 
 public interface ItemRepository extends ListCrudRepository<Item, Long> {
 
-    List<Item> findByType(String type);
+    List<Item> findByType(ItemType type);
 
     List<Item> findByNameContainingIgnoreCase(String name);
 }
